e9c57b9f457f5f1280b748d8df002c60dd8877a1
Marco <marco.canovese@studenti.unipd.it>
2021-04-18 16:49:01 +0200

    dynamo

---
+++ src/repository/dynamo.ts
@@ -0,0 +1,106 @@
+import { Product } from "src/core/product";
+import { Persistence } from "src/repository/persistence";
+import * as AWS from "aws-sdk";
+
+export class Dynamo implements Persistence {
+
+    private static readonly TABLE_PRODUCTS = "products";
+    private DOCUMENT_CLIENT = new AWS.DynamoDB.DocumentClient({ region: "eu-central-1" });
+
+    public async getAll (): Promise<Array<Product>> {
+        const PARAMS = {
+            TableName: Dynamo.TABLE_PRODUCTS
+        };
+
+        const DATA = await this.DOCUMENT_CLIENT.scan(PARAMS).promise();
+        console.log("Data from DB: " + JSON.stringify(DATA));
+        const PRODUCTS = new Array<Product>();
+        DATA.Items.forEach(element => {
+            PRODUCTS.push(new Product(element.id, element.name, element.description, element.primaryPhoto, element.secondaryPhoto, element.category, 
+                element.price, element.netPrice, element.tax, element.show, element.showHome, element.stock));
+        });
+
+        return PRODUCTS ;
+    } 
+
+    public async getItem (id: string): Promise<Product> {
+        const PARAMS = {
+            Key: {
+                id: id
+            },
+            TableName: Dynamo.TABLE_PRODUCTS,
+            IndexName: "id-index"
+        };
+
+        const DATA = await this.DOCUMENT_CLIENT.get(PARAMS).promise();
+        return DATA.Item? new Product(DATA.Item.id, DATA.Item.name, DATA.Item.description, DATA.Item.primaryPhoto, DATA.Item.secondaryPhoto, DATA.Item.category, 
+            DATA.Item.price, DATA.Item.netPrice, DATA.Item.tax, DATA.Item.show, DATA.Item.showHome, DATA.Item.stock) : null;
+    }
+
+    public async addItem (item: Product): Promise<boolean> {
+        const PARAMS = {
+            TableName: Dynamo.TABLE_PRODUCTS,
+            Key: {
+                id: item.getID()
+            },
+            Item: item
+        };
+
+       const DATA =  this.DOCUMENT_CLIENT.put(PARAMS).promise().catch(
+            () => {return false; }
+       );
+
+       return (DATA) ? true : false;
+
+    }
+
+    public async editItem (item: Product): Promise<boolean> {
+        const VALUES = {};
+        let expression = "SET ";
+        let first = true;
+
+        Object.keys(item).forEach(function (key) {
+            if (key != "id") {
+                const VALUE = item[key];
+                if (!first) {
+                    expression += ", "
+                } 
+                else {
+                    first = false;
+                }
+                expression += key + " = :" + key;
+                VALUES[":" + key] = VALUE;
+            }
+        });
+
+        const PARAMS = {
+            TableName: Dynamo.TABLE_PRODUCTS,
+            Key: {
+                id: item.getID()
+            },
+            UpdateExpression: expression,
+            ExpressionAttributeValues: VALUES
+        }
+        console.log(PARAMS);
+
+        const DATA = await this.DOCUMENT_CLIENT.update(PARAMS).promise().catch(
+            (err) => { return err; }
+        );
+        return DATA;
+    }
+
+    public async deleteItem (id: string): Promise<boolean> {
+        const PARAMS = {
+            Key: {
+                id: id
+            },
+            TableName: Dynamo.TABLE_PRODUCTS,
+            IndexName: "id-index"
+        };
+
+       await this.DOCUMENT_CLIENT.delete(PARAMS).promise().catch(
+            (err) => { return err; }
+        );
+        return true;;      
+    }
+}
