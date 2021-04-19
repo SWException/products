12c0be7be7c4c594de88a3d555c07c36d053dcce
ftrole <francescotrolese99@gmail>
2021-04-19 16:38:38 +0200

    ciao

--- src/repository/taxes/taxesService.ts
+++ src/repository/taxes/taxesService.ts
@@ -3,11 +3,9 @@ import { Taxes } from "./taxes";
 
 export class TaxesService implements Taxes {
     public async getTax (id: string): Promise<any> {
-        return await fetch(process.env.SERVICES + `/taxes/${id}`)
-            .then(async response => {
-                const RESPONSE = response.json();
-                console.log(RESPONSE["data"]);
-                return RESPONSE["data"] ? RESPONSE["data"] : null;
-            })
+        const RESPONSE = await fetch(process.env.SERVICES + `/taxes/${id}`);
+        const BODY= await RESPONSE.json();
+        console.log(BODY);
+        return BODY.data? BODY.data : null;
     }
 }
