576891347671ab51bc4587410f5e8792d2df51c7
ftrole <50705762+ftrole@users.noreply.github.com>
2021-04-20 16:05:16 +0200

    Update updateStock.ts

--- src/handlers/updateStock.ts
+++ src/handlers/updateStock.ts
@@ -8,20 +8,25 @@ export const HANDLER: APIGatewayProxyHandler = async (event) => {
     if (TOKEN == null) {
         return API_RESPONSES._400(null, "error", "missing authentication token");
     }
-    const QUANTITY = event.pathParameters?.quantity as unknown as number;
+    const DATA = JSON.parse(event?.body);
+    const QUANTITY = DATA.quantity as number;
+    console.log(QUANTITY);
     if (!QUANTITY) {
-        return API_RESPONSES._400(null, "error", "error during the permissions check")
+        return API_RESPONSES._400(null, "error", "a quantity must be provided")
     }
     //check for the id of the product 
     const PRODUCT_ID: string = event.pathParameters?.id;
     if (!PRODUCT_ID) {
         return API_RESPONSES._400(null, null, "id prodotto non presente");
     }
-
-    const MODEL: Model = Model.createModel();
-    const RES = await MODEL.changeStock(PRODUCT_ID, QUANTITY, TOKEN);
-
-    console.log(JSON.stringify(RES));
-
-    return API_RESPONSES._200(JSON.parse(JSON.stringify(RES)));
+    try{
+        const MODEL: Model = Model.createModel();
+        const RES = await MODEL.changeStock(PRODUCT_ID, QUANTITY, TOKEN);
+        console.log(JSON.stringify(RES));
+        return API_RESPONSES._200(JSON.parse(JSON.stringify(RES)));
+    }
+    catch(err) {
+        console.log(err.message);
+        return API_RESPONSES._400(null, "error", err.message);
+    }
 }
